85e419acc94f4014d65fd46b82b18519c4d09816
Vitor Lassen <vitor@lassen.com.br>
2018-09-22 14:38:23 -0300

    aula 15-09

--- app/src/main/java/com/example/vitor/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/vitor/tipcalculator/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.vitor.tipcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NumberFormat currentFormat = NumberFormat.getCurrencyInstance();
+    private NumberFormat percentFormat = NumberFormat.getPercentInstance();
+    private TextView amountTextView;
+    private TextView percenteTextView;
+    private TextView tipTextView;
+    private TextView totalTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        amountTextView = findViewById(R.id.amountTextView);
+        percenteTextView = findViewById(R.id.percentTextView);
+        tipTextView = findViewById(R.id.tipTextView);
+        totalTextView = findViewById(R.id.totalTextView);
+
+
     }
 }
